d65624986afdb94ee620744c6f33d83b9ccd6686
surgiai <127158314+surgiai@users.noreply.github.com>
2023-03-21 00:58:52 -0500

    push

---
+++ neural_network.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE mydatabase;
+USE mydatabase;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
